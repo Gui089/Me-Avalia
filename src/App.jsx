199181ea60f48b885770eb7bca46cbbcd1d852cd
Guilherme <guilhermesilba811@gmail.com>
2023-12-15 21:32:42 -0300

    Nav bar feita

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "./Components/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export { App };
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <img className="logo" src="./img/logo-me-avalia.png" alt="" />
+      <form className="form-search" action="">
+        <input className="search" placeholder="Buscar filmes" />
+        <button className="btn-search">Buscar</button>
+      </form>
+      <h2>5 Resultados</h2>
+    </div>
+  );
+};
+
+export { NavBar };
