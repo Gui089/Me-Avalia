cea9476dca6d3c5f89d5a8495af43796b583e1e3
Guilherme <guilhermesilba811@gmail.com>
2024-01-03 09:11:42 -0300

    strict mode removido, menos chamdas na API

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
@@ -7,8 +6,4 @@ const rootElement = document.querySelector('[data-js="root"]');
 
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+root.render(<App />);
